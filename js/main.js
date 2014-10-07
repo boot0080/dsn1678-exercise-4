d3d5f6aad740dca545c32724b0e36493c7dfd03c
Ariah Boots <boot0080@algonquinlive.com>
2014-10-07 09:59:34 -0400

    added animations and images

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 var $win = $(window);
 var $sun = $('.sun');
+var $sunSection = $('.sun-section');
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
  
-	$sun.css('transform', 'rotate(' + scrollPos + 'deg)');
+	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-postion', 'center ' + scrollPos / 3 + 'px');
 });
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, {offset: '50%'});
